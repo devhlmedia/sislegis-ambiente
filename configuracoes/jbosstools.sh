a1b268b4f96c2b6ab12a59ed9f8406a6688ec5cd
Paulo Jerônimo <paulojeronimo@gmail.com>
2014-12-11 11:01:02 -0200

    Atualização do JBoss Tools

--- configuracoes/jbosstools.sh
+++ configuracoes/jbosstools.sh
@@ -1,2 +1,2 @@
-JBOSSTOOLS_INSTALADOR=jbosstools-4.2.0.CR1_2014-09-14_02-42-22-B242-updatesite-core.zip
+JBOSSTOOLS_INSTALADOR=jbosstools-4.2.0.Final_2014-10-17_02-38-33-B287-updatesite-core.zip
 JBOSSTOOLS_INSTALADOR_URL=http://ufpr.dl.sourceforge.net/project/jboss/JBossTools/jbosstools4.2.x
