6466f6e4264575995b934d5587bfbcafa3b29dbb
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-01-28 01:53:08 +0000

    Atualização da versão do JDK

--- configuracoes/Linux/java.sh
+++ configuracoes/Linux/java.sh
@@ -1,6 +1,6 @@
 distro=`distro`
 case $distro in
-    CentOS|Fedora) export JAVA_HOME=/usr/java/jdk1.8.0_25;;
+    CentOS|Fedora) export JAVA_HOME=/usr/java/jdk1.8.0_31;;
     Ubuntu) export JAVA_HOME=/usr/lib/jvm/java-8-oracle;;
     *) echo "Configure JAVA_HOME para a distribuição \"$distro\"!"
 esac
