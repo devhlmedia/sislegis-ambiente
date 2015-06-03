5c2bb74df5eec03c50f151bf24ee0a398dde615e
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-06-03 10:45:35 -0300

    Mudança p/ OpenJDK no Fedora 21 devido a problemas na instalação do Oracle JDK através do Fedy

--- configuracoes/Linux/java.sh
+++ configuracoes/Linux/java.sh
@@ -1,6 +1,12 @@
 distro=`distro`
 case $distro in
-    CentOS|Fedora) export JAVA_HOME=/usr/java/latest;;
+    CentOS|Fedora) 
+        # Alternativa 1) OpenJDK
+        export JAVA_HOME=/usr/lib/jvm/java
+
+        # Alternativa 2) Oracle JDK
+        #export JAVA_HOME=/usr/java/latest
+        ;;
     Ubuntu) export JAVA_HOME=/usr/lib/jvm/java-8-oracle;;
     *) echo "Configure JAVA_HOME para a distribuição \"$distro\"!"
 esac
