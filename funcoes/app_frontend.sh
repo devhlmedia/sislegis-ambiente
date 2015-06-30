8ebabb54ad6b3a035de9684498bcea71e4360535
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-06-30 17:22:42 +0000

    Ajustes para o funcionamento da separaço do frontend

--- funcoes/app_frontend.sh
+++ funcoes/app_frontend.sh
@@ -1,4 +1,7 @@
 app_frontend() { cd "$APP_FRONTEND_HOME"; }
 app_frontend_baixar() { baixar-app_frontend; }
+app_frontend_remote_add_upstream() { 
+    (app_frontend && git remote add upstream http://github.com/pensandoodireito/sislegis-app-frontend)
+}
 
 # vim: set ts=4 sw=4 expandtab:
