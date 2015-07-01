8c0e59f454f69a792e1ad6e0e30e7c91d06d021f
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-07-01 17:32:01 +0000

    Ajustes de configuração da aplicação dependendo do ambiente

--- configuracoes/app.sh
+++ configuracoes/app.sh
@@ -1,22 +1,37 @@
 export APP_HOME=$PROJETOS_DIR/app
+APP_AMBIENTE=${APP_AMBIENTE:-desenvolvimento}
 
 case `uname` in
   Linux)
     case `distro` in
+      # Fedora e Ubuntu são utilizados apenas em ambiente de desenvolvimento.
+      #   Por esse motivo, os valores default (acima configurados) não precisam ser alterados
       Fedora|Ubuntu)
-        APP_HOST=sislegis.local
-        APP_IP=127.0.0.1
+        APP_HOST=${APP_HOST:-sislegis.local}
+        APP_IP=${APP_IP:-127.0.0.1}
         ;;
+      # CentOS é utilizado em ambientes de desenvolvimento, homologação e produção.
+      #   Obs: este projeto não é utilizado para configurar o ambiente de produção
+      #   Para esse ambiente é utilizado o projeto sislegis-ambiente-producao
       CentOS)
-        APP_HOST=homologacao-sislegis.pensandoodireito.mj.gov.br
-        APP_IP=172.17.6.80
+        case "$APP_AMBIENTE" in
+          desenvolvimento)
+            APP_HOST=${APP_HOST:-sislegis.local}
+            APP_IP=${APP_IP:-127.0.0.1}
+            ;;
+          homologacao)
+            # Ajusta as configurações default para o ambiente de homologação
+            APP_HOST=${APP_HOST:-homologacao-sislegis.pensandoodireito.mj.gov.br}
+            APP_IP=${APP_IP:-172.17.6.80}
+        esac
         ;;
     esac
     ;;
-  Darwin)
-    APP_HOST=sislegis.local
-    APP_IP=127.0.0.1
-    ;;
+  # Para outros sistemas operacionais (Darwin, Cygwin), os valores default são suficientes
+  *)
+    APP_HOST=${APP_HOST:-sislegis.local}
+    APP_IP=${APP_IP:-127.0.0.1}
+    ;;    
 esac
 
 # vim: set ts=2 sw=2 expandtab:
