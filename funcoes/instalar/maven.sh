2a010b567d06ed148cf161db5b5cb124e0141a99
Rafael Coutinho <rafaelcoutinho@gmail.com>
2016-01-15 15:13:10 -0200

    Fix env

--- funcoes/instalar/maven.sh
+++ funcoes/instalar/maven.sh
@@ -2,14 +2,17 @@ instala_maven() {
     local settings_dir="$HOME"/.m2
     instala_aplicacao
 
-    if [ "$USA_PROXY" ]
-    then
-        mkdir -p "$settings_dir"
+ mkdir -p "$settings_dir"
         cat > "$settings_dir"/settings.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" 
           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
           xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+EOF
+
+    if [ "$USA_PROXY" ]
+    then
+        cat >> "$settings_dir"/settings.xml <<EOF
   <proxies>
     <proxy>
       <id>MJ</id>
@@ -21,9 +24,30 @@ instala_maven() {
       <port>$HTTP_PROXY_PORT</port>
     </proxy>
   </proxies>
-</settings>
 EOF
     fi
+
+ cat >>  "$settings_dir"/settings.xml <<EOF
+ <profiles>
+      <profile>
+            <id>sislegis</id>
+            <properties>
+               <keycloak.server>localhost</keycloak.server>
+            </properties>
+        <activation>
+                                <activeByDefault>true</activeByDefault>
+                                <jdk>1.7</jdk>
+                        </activation>
+
+                </profile>
+        </profiles>
+    <activeProfiles>
+       <activeProfile>sislegis</activeProfile>
+    </activeProfiles>
+</settings>
+
+EOF
+
 }
 
 remove_maven() {
